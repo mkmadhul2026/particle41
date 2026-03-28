50ba428d50c96a5c215becb8ab294353f6876c8b
Madhul Moni Kamalam <mkmadhul@outlook.com>
2026-03-28 20:53:42 +0530

    particle41 assessment commit restructured

---
+++ terraform/main.tf
@@ -0,0 +1,59 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+############################
+# VPC
+############################
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.0"
+
+  name = "eks-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs = ["ap-south-1a", "ap-south-1b"]
+
+  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  enable_nat_gateway = true
+  single_nat_gateway = true
+
+  tags = {
+    "kubernetes.io/cluster/eks-cluster" = "shared"
+  }
+}
+
+############################
+# EKS Cluster
+############################
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.0.0"
+
+  cluster_name    = "eks-cluster"
+  cluster_version = "1.29"
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets   # ✅ private only
+
+  ############################
+  # Node Group
+  ############################
+  eks_managed_node_groups = {
+    default = {
+      desired_size = 2
+      min_size     = 2
+      max_size     = 2
+
+      instance_types = ["m6a.large"]
+
+      subnet_ids = module.vpc.private_subnets  # ✅ private only
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+  }
+}
